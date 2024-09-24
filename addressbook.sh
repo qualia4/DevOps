edf897bcc4b4a94dcf1521fc08d9fa9bc37c657d
Maxim Pozdniakov <mpozdniakov@kse.org.ua>
2024-09-24 22:33:52 +0300

    address book code updated

---
+++ addressbook.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ADDRESS_BOOK_TXT="address_book.txt"
+
+function add_contact() {
+    echo "Enter contact information:"
+    read -p "Name: " name
+    read -p "Phone: " phone
+    read -p "Email: " email
+
+    echo "$name,$phone,$email" >> "$ADDRESS_BOOK_TXT"
+    echo "Contact added successfully."
+}
+
+function remove_contact() {
+    read -p "Enter name, phone, or email of the contact to remove: " search_term
+    grep -iq "$search_term" "$ADDRESS_BOOK_TXT" && {
+        sed -i "/$search_term/d" "$ADDRESS_BOOK_TXT"
+        echo "Contact removed successfully."
+    } || echo "No contact found matching '$search_term'."
+}
+
+function search_contacts() {
+    read -p "Enter search term: " search_term
+    grep -i "$search_term" "$ADDRESS_BOOK_TXT" | while IFS=',' read -r name phone email; do
+        echo "Name: $name"
+        echo "Phone: $phone"
+        echo "Email: $email"
+        echo "---"
+    done
+}
+
+case "$1" in
+    add)
+        add_contact
+        ;;
+    search)
+        search_contacts
+        ;;
+    remove)
+        remove_contact
+        ;;
+    *)
+        echo "Wrong command"
+        ;;
+esac
